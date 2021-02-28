deaf03df252193bbedece8295fbc72c12b24bcd7
meta <meta@radium.homelab.bsdgirl.net>
2021-02-28 22:09:10 +1100

    Capture groups, yay

--- cogs/dice.py
+++ cogs/dice.py
@@ -9,15 +9,12 @@ class Dice_Commands(commands.Cog, name='Dice Commands'):
         self.bot.help_command.cog = self
     @commands.command(help=f"Roll some dice")
     async def roll(self, ctx, dice):
-        if re.match('^\d{1,2}[dD]\d{1,3}$', dice):
-            # dice_types = [2, 4, 6, 8, 10, 12, 20, 100]
+        arg = re.search('^(\d{1,2})[dD](\d{1,3})$', dice)
+        if arg:
             arg = re.split('d', dice, flags=re.IGNORECASE)
             num_dice = int(arg[0])
             dice_type = int(arg[1])
             rolls = []
-            # if dice_type not in dice_types:
-            #     await ctx.send(f'Please specify a valid dice type')
-            #     return False
             for i in range(0, num_dice):
                 rolls.append(random.randint(1, dice_type))
             await ctx.send(f'Rolling {dice} \nResults: {", ".join(str(x) for x in rolls)}')
